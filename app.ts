698a2c1b5608727c7eada48f76f76626eab3df79
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-05 08:52:17 +0700

    array types

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "Rendi",
   age: 34,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Swimming", "Basketball"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
