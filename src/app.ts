c22ee6d1ddd1ae6ac373de6893131a6a7aa8299c
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 17:06:23 +0700

    interface as function types

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+// type addFn = (a: number, b: number) => number;
+// let add: addFn;
+// add = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
   readonly name: string;
 }
