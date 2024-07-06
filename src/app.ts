caa2b9e9c79f4b4c049065bbfd4158ece7aafe14
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 15:01:32 +0700

    shorthand initialization

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-  public name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = name;
   }
 
   describe() {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +22,7 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("dept1", "Accounting");
 
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
