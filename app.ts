10eeff4a3e5321b81321cccc6f0361f342c01a12
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 12:01:08 +0700

    unknown types

--- app.ts
+++ app.ts
@@ -1,24 +1,6 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
-}
+let userInput: unknown;
+let userName: string;
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 18));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") userName = userInput;
