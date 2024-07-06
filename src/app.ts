79bfd73a2dd4ab62fd89748c2dff2c80dda42a4e
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 13:44:51 +0700

    strict options

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 const button = document.querySelector("button")!;
 
-button.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+function clickHandler(message: string) {
+  console.log("Clicked! " + message);
+}
+
+if (button) button.addEventListener("click", clickHandler.bind(null));
