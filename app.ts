ceb336de3b69775603a06c4424795907cf655532
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 12:49:34 +0700

    never types

--- app.ts
+++ app.ts
@@ -4,3 +4,9 @@ let userName: string;
 userInput = 5;
 userInput = "Max";
 if (typeof userInput === "string") userName = userInput;
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occured!", 500);
