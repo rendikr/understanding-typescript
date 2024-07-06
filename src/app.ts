4327705a97f3b1096668aea0d07b10acc490c7ef
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-06 17:36:04 +0700

    type guards

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ type Employee = {
   startDate: Date;
 };
 
+// The & symbol indicates an intersection types
 type ElevatedEmployee = Admin & Employee;
 
 const employee1: ElevatedEmployee = {
@@ -16,9 +17,63 @@ const employee1: ElevatedEmployee = {
   startDate: new Date(),
 };
 
-console.log(employee1);
-
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    // this is called type guards
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(employee: UnknownEmployee) {
+  console.log(`Name: ${employee.name}`);
+  if ("privileges" in employee) {
+    // check if "privileges" property is exists in employee
+    console.log(`Privileges: ` + employee.privileges);
+  }
+  if ("startDate" in employee) {
+    // check if "privileges" property is exists in employee
+    console.log(`Start Date: ` + employee.startDate);
+  }
+}
+
+console.log(employee1);
+printEmployeeInformation(employee1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ... " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
