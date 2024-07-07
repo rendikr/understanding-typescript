dcfe4a6115db461cd5c34e04ef8f0d5673d7176c
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-07 21:46:48 +0700

    index properties

--- src/app.ts
+++ src/app.ts
@@ -118,3 +118,12 @@ const userInputElement2 = document.getElementById(
 )! as HTMLInputElement;
 
 userInputElement2.value = "Hi!";
+
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  username: "Must start with a capital character!",
+};
