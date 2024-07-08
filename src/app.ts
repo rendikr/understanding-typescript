164e8b6f2ceb4ad974be6c746da367c9515aadb8
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-08 18:07:00 +0700

    decorator in class property

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,32 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) this._price = val;
+    else throw new Error("Invalid price - should be positive!");
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
