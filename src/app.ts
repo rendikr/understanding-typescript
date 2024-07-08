8d97ad7b24f25090905c784dec1969c38fba0eee
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-08 21:23:58 +0700

    rendering project list

--- src/app.ts
+++ src/app.ts
@@ -58,6 +58,45 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content, // points to the contents inside the <template></template> tag
+      true
+    );
+    // set the first tag after templateElement as "this.element"
+    // in this case, this.element referes to the <section></section> tag
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  // insert the templateElement before the closing tag of hostElement tag
+  // with hostElement is set as <div id="app"></div>, then:
+  // <div id="app">templateElement</div>
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -78,6 +117,7 @@ class ProjectInput {
       true
     );
     // set the first tag after templateElement as "this.element"
+    // in this case, this.element referes to the <form></form> tag
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
 
@@ -164,3 +204,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
