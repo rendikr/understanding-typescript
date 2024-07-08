58e4207accbd105e42a7784aeaf968ded96a605f
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-08 07:58:25 +0700

    creating generics function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Well Done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Well Done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  // data will be a type string because the return of the Promise is a string
-  console.log(data.split(" "));
-});
+// promise.then((data) => {
+//   // data will be a type string because the return of the Promise is a string
+//   console.log(data.split(" "));
+// });
+
+function merge<T extends object, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj.name);
+console.log(mergedObj.age);
