53c5b8016164116e88067e56b25006df1dbf3b28
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-08 08:04:08 +0700

    constraints

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@
 //   console.log(data.split(" "));
 // });
 
-function merge<T extends object, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
