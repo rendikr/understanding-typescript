1d8589a65c88bb2fcf1220c29a3f4eeab7a199ae
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-08 20:40:55 +0700

    interacting with DOM element

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -13,10 +16,38 @@ class ProjectInput {
       this.templateElement.content, // points to the contents inside the <template></template> tag
       true
     );
+    // set the first tag after templateElement as "this.element"
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    // called when the form is submitted
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // this.element bind to the form element
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  // insert the templateElement after the beginning of hostElement tag open
+  // with hostElement is set as <div id="app"></div>, then:
+  // <div id="app">templateElement</div>
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
