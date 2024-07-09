89edcfb54f56fd2012bb0205c4d533523ef5dc80
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-09 07:53:42 +0700

    using es modules

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,50 +1,45 @@
-namespace App {
-  // Component Base Class
-  export abstract class Component<
-    T extends HTMLElement,
-    U extends HTMLElement
-  > {
-    templateElement: HTMLTemplateElement;
-    hostElement: T;
-    element: U;
+// Component Base Class
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string
-    ) {
-      this.templateElement = document.getElementById(
-        templateId
-      )! as HTMLTemplateElement;
-      this.hostElement = document.getElementById(hostElementId)! as T;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      const importedNode = document.importNode(
-        this.templateElement.content, // points to the contents inside the templateElement tag
-        true
-      );
-      // set the first tag after templateElement as "this.element"
-      // in this case, this.element referes to the <section></section> tag
-      this.element = importedNode.firstElementChild as U;
-      if (newElementId) {
-        this.element.id = newElementId;
-      }
-
-      this.attach(insertAtStart);
+    const importedNode = document.importNode(
+      this.templateElement.content, // points to the contents inside the templateElement tag
+      true
+    );
+    // set the first tag after templateElement as "this.element"
+    // in this case, this.element referes to the <section></section> tag
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
     }
 
-    private attach(insertAtStart: boolean) {
-      // insert the templateElement before the closing tag of hostElement tag
-      // with hostElement is set as <div id="app"></div>, then:
-      // <div id="app">templateElement</div>
-      this.hostElement.insertAdjacentElement(
-        insertAtStart ? "afterbegin" : "beforeend",
-        this.element
-      );
-    }
-
-    abstract configure(): void;
-
-    abstract renderContent(): void;
+    this.attach(insertAtStart);
   }
+
+  private attach(insertAtStart: boolean) {
+    // insert the templateElement before the closing tag of hostElement tag
+    // with hostElement is set as <div id="app"></div>, then:
+    // <div id="app">templateElement</div>
+    this.hostElement.insertAdjacentElement(
+      insertAtStart ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+
+  abstract renderContent(): void;
 }
--- src/models/project.ts
+++ src/models/project.ts
@@ -1,17 +1,15 @@
-namespace App {
-  // Project Type Class
-  export enum ProjectStatus {
-    Active,
-    Finished,
-  }
+// Project Type Class
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
 
-  export class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) {}
-  }
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
 }
