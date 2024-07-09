cd4d47edbe7db6c03f31f86d7e2f79bef95c696e
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-09 09:09:53 +0700

    using class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-import _ from "lodash";
+import { Product } from "./product.model";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 
-console.log(_.shuffle([1, 2, 3, 4, 5]));
+const products = [
+  { title: "Carpet", price: 29.99 },
+  { title: "Book", price: 12.99 },
+];
+
+// the old way
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+// using class-transformer
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
