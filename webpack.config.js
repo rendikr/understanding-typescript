7999a9be7c4696f2be6449d9e7937eb1ed53ebea
Rendi K. <rendi.krinaldi@gmail.com>
2024-07-09 08:39:25 +0700

    adding webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
-}
+    extensions: ['.ts', '.js'],
+  },
+};
